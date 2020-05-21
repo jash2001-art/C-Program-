b9651a308c4da87ee65dfd07a7b94c974bb06d48
jash2001-art <63535402+jash2001-art@users.noreply.github.com>
2020-05-22 00:24:48 +0530

    Scoresheet created on C
    Created by Jash Patel

---
+++ Scoresheet.c
@@ -0,0 +1,35 @@
+#include<"declaration.h">
+#include<"welcome.h">
+#include<"menu.h">
+#include<"newscoresheet.h">
+#include"filing.h"
+#include"final.h"
+int main()
+{
+	system("color f1");
+int a,key;
+char b;
+File *fnew;
+char lastinput;
+while(True)
+{
+welcome();
+system("cls");
+menu()
+ch=getch();
+char ch;
+switch(ch)
+{
+	case'1';
+	fileopen(ch);
+	case'2';
+	fileopen(ch);
+	getch();
+	case'3';
+	system('cls');
+	exit(0);
+	}
+	}
+	}
+	
+
